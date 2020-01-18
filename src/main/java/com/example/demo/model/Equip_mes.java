529084a8e43eb7776008a3ed3e28bc201abdfdce
ywx <909897268@qq.com>
2020-01-18 17:49:46 +0800

    +

---
+++ src/main/java/com/example/demo/model/Equip_mes.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Equip_mes {
+    int id;
+    int equip_id;
+    int attack;
+    int phy_def;
+    int value;
+    String derails;
+    String quality;
+
+}
---
+++ src/main/java/com/example/demo/model/Game_data.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Game_data {
+    int id;
+    String user_id;
+    String username;
+    int strength;
+    int fight_count;
+    String equipment;
+    int money;
+    String career;
+    String equipped;
+    Date res_date;
+    Date online_time;
+
+}
