5267a44b8056650b31cf1030bc84beabcb268ceb
cxl <1799359457@qq.com>
2020-01-20 23:57:44 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/example/demo/dao/UsersMapping.java
    #	src/main/java/com/example/demo/service/UsersService.java
    #	src/main/resources/mapper/UsersMapper.xml

--- src/main/java/com/example/demo/service/imp/UsersServiceImp.java
+++ src/main/java/com/example/demo/service/imp/UsersServiceImp.java
@@ -11,16 +11,13 @@ public class UsersServiceImp implements UsersService {
 
     @Autowired
     private UsersMapping users;
-
     @Override
-    public User Login(String g_id, String pwd) {
-        return users.Login(g_id, pwd);
+    public User Login(int g_id, String g_pwd) {
+        return users.Login(g_id, g_pwd);
     }
 
     @Override
-    public int Register(int uid,String u_id, String pwd, String name) {
-        return users.Register(uid,u_id, pwd, name);
+    public int Register(int g_id, String g_pwd) {
+        return users.Register(g_id,g_pwd);
     }
-
-
 }
