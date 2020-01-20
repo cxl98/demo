a249218c838206741a9a5cdf514e20140448fac6
ywx <909897268@qq.com>
2020-01-20 22:14:01 +0800

    1

--- src/main/java/com/example/demo/control/UsersController.java
+++ src/main/java/com/example/demo/control/UsersController.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.JSONObject;
 import com.example.demo.model.User;
 import com.example.demo.service.imp.UsersServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
